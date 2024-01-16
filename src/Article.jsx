1cb119ef68216610e0973fdbb2c7d8724d8f8a14
Barla Ankit <barla.x88@gmail.com>
2024-01-16 14:24:02 +0530

    Change styles for remove article btn

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-
 const Article = ({ title, img, link, text, uid, removeArticle }) => {
-  const [showFullText, setShowFullText] = useState(false);
-
   return (
     <div
       className="article"
@@ -18,29 +14,17 @@ const Article = ({ title, img, link, text, uid, removeArticle }) => {
           <img src={img} alt="post img" />
         </a>
       </div>
-      <div className="article-text">
-        {showFullText ? text : text.substring(0, 250) + ' '}
-        <span
-          className="expand"
-          onClick={() => {
-            setShowFullText(!showFullText);
-          }}
-        >
-          {' '}
-          show {showFullText ? 'Less' : 'More'}..
-        </span>
-      </div>
+      <div className="article-text">{text.substring(0, 250)}</div>
       <button
         type="button"
         className="btn"
         onClick={() => {
-          if (showFullText) setShowFullText(false);
           setTimeout(() => {
             removeArticle(uid);
           }, 600);
         }}
       >
-        Not Interested
+        X
       </button>
     </div>
   );
